b746ce9e4f770f93ab8458fdf43218509b53d0ee
Angad Sahni <sahni.angad@outlook.com>
2026-08-24 13:51:52 +0100

    table created in index.html & adding tasks successful

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, request, url_for, render_template
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime, timezone
 
@@ -8,7 +8,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 # Connect SQLAlchemy to this Flask app.
 db = SQLAlchemy(app)
 
-# Database model for a single task in the to-do list.
+# Database model for a single task in the to-do list; each to-do task is a row in the database, site.db.
+## class Todo defines which components of each task should be stored (i.e. columns in the database table).
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False) # nullable=False means that this column cannot be empty.
@@ -24,8 +25,22 @@ class Todo(db.Model):
 # GET = browser loads this page.
 # POST = browser sends form data to this route, e.g. text from an "add task" input.
 @app.route("/", methods=['POST', 'GET'])
-def index(): 
-    return render_template("index.html") 
+def index():
+    if request.method == 'POST': # if user submitted the form (defined in index.html)
+        task_content = request.form["content"] # get the text from the form input named 'content'.
+        new_task = Todo(content=task_content) # create a new task with the text from the form.
+
+        try:
+            db.session.add(new_task) # add the new task to the database session.
+            db.session.commit() # commit the session to save the new task to the database.
+            return redirect("/") # redirect to the index page to show the updated list of tasks.
+        except:
+            return "There was an issue adding your task."
+
+    else: # no task entered.
+        tasks = Todo.query.order_by(Todo.date_created).all() # get 'all' tasks from the database, ordered by creation date.
+        return render_template("index.html", tasks=tasks) # just load the index.html page.
+        ## tasks=tasks passes the list of tasks to the HTML template, so it can display them. 'tasks' variable will be aded to the index.html template.
 
 
 
